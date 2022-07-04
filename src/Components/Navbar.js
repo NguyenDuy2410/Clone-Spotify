3badfc6d17563efa09e9da708e27d555f137cfe0
NguyenDuy2410 <nvlduy24102000@gmail.com>
2022-07-04 23:37:45 +0700

    first commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react";
 
 export default function Navbar() {
-  return (
-    <div className='h-24 bg-slate-900 text-white text-center leading-[6rem] text-3xl '>
-        <i class="fa fa-spotify mx-3" ></i>
-        Spotify
-    </div>
-  )
+    return (
+        <div className="h-24 bg-slate-900 text-white text-center leading-[6rem] text-3xl">
+            <i className="fa fa-spotify mr-5"></i>
+            Spotify
+        </div>
+    );
 }
